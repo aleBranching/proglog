4b72ee207ab4c7ed60afa4c6be3528553d403a8b
aleBranching <aleksas773@googlemail.com>
2025-05-11 11:39:28 +0100

    index implemented and tested

--- internal/log/index.go
+++ internal/log/index.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"github.com/tysonmote/gommap"
+	"io"
 	"os"
 )
 
@@ -26,7 +27,7 @@ func newIndex(f *os.File, c Config) (*index, error) {
 	}
 
 	idx.size = uint64(fi.Size())
-	err = os.Truncate(fi.Name(), int64(c.Segment.MaxIndexBytes))
+	err = os.Truncate(f.Name(), int64(c.Segment.MaxIndexBytes))
 	if err != nil {
 		return &index{}, err
 	}
@@ -38,4 +39,55 @@ func newIndex(f *os.File, c Config) (*index, error) {
 	return idx, nil
 }
 
-func (i *)
+func (i *index) Close() error {
+	if err := i.mmap.Sync(gommap.MS_SYNC); err != nil {
+		return err
+	}
+	if err := i.file.Truncate(int64(i.size)); err != nil {
+		return err
+	}
+	if err := i.file.Sync(); err != nil {
+		return err
+	}
+	if err := i.file.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
+	if i.size == 0 {
+		return 0, 0, io.EOF
+	}
+	// changing naming
+	var indexEntry uint32
+	if in == -1 {
+		indexEntry = uint32((i.size / entWidth) - 1)
+	} else {
+		indexEntry = uint32(in)
+	}
+	var indexEntryBytePosition uint64 = uint64(indexEntry) * entWidth
+
+	if i.size < indexEntryBytePosition+entWidth {
+		return 0, 0, io.EOF
+	}
+
+	out = enc.Uint32(i.mmap[indexEntryBytePosition : indexEntryBytePosition+offWidth])
+	pos = enc.Uint64(i.mmap[indexEntryBytePosition+offWidth : indexEntryBytePosition+entWidth])
+
+	return out, pos, nil
+}
+
+func (i *index) Write(off uint32, pos uint64) error {
+	if uint64(len(i.mmap)) < i.size+entWidth {
+		return io.EOF
+	}
+	enc.PutUint32(i.mmap[i.size:i.size+offWidth], off)
+	enc.PutUint64(i.mmap[i.size+offWidth:i.size+entWidth], pos)
+	i.size += entWidth
+	return nil
+}
+
+func (i *index) Name() string {
+	return i.file.Name()
+}
--- internal/log/index_test.go
+++ internal/log/index_test.go
@@ -1,7 +1,129 @@
 package log
 
-var (
-	offWidth uint64 = 4
-	posWidth uint64 = 8
-	entWidth        = offWidth + posWidth
+import (
+	"os"
+	"testing"
 )
+
+//var (
+//	offWidth uint64 = 4
+//	posWidth uint64 = 8
+//	entWidth        = offWidth + posWidth
+//)
+
+func TestIndex(t *testing.T) {
+
+	f, err := os.CreateTemp("", "index")
+	if err != nil {
+		t.Errorf("Errored")
+	}
+
+	c := Config{}
+	c.Segment.MaxIndexBytes = 1024
+
+	idx, err := newIndex(f, c)
+	if err != nil {
+		t.Fatal("couldn't create an index: \n", err)
+	}
+	_, _, err = idx.Read(-1)
+	if err == nil {
+		t.Errorf("Should have failed")
+	}
+	_, _, err = idx.Read(0)
+	if err == nil {
+		t.Errorf("Should have failed")
+	}
+
+	entries := []struct {
+		pos    uint32
+		offset uint64
+	}{
+		{
+			pos:    uint32(0),
+			offset: uint64(0),
+		},
+		{
+			pos:    uint32(1),
+			offset: uint64(10),
+		},
+	}
+
+	for _, entry := range entries {
+		err = idx.Write(entry.pos, entry.offset)
+		if err != nil {
+			t.Errorf("Error %s", err)
+		}
+		pos, off, err := idx.Read(int64(entry.pos))
+		if err != nil {
+			t.Errorf("errored")
+		}
+		if entry.pos != pos || entry.offset != off {
+			t.Errorf("mismatch")
+		}
+
+	}
+	_, _, err = idx.Read(int64(len(entries)))
+	if err == nil {
+		t.Fatalf("Should have errored")
+	}
+	err = idx.Close()
+	if err != nil {
+		t.Fatalf("Can;t close")
+	}
+	//restoring an index
+	f, _ = os.OpenFile(f.Name(), os.O_RDWR, 0600)
+	idx, err = newIndex(f, c)
+	if err != nil {
+		t.Fatal("couldn't create an index: \n", err)
+	}
+	pos, off, err := idx.Read(-1)
+	if err != nil {
+		t.Errorf("failed to read")
+	}
+	if pos != entries[1].pos || off != entries[1].offset {
+		t.Errorf("Should have matched %v : %v	%v : %v", pos, entries[1].pos, off, entries[1].offset)
+	}
+	for _, entry := range entries {
+		pos, off, err := idx.Read(int64(entry.pos))
+		if err != nil {
+			t.Errorf("errored")
+		}
+		if entry.pos != pos || entry.offset != off {
+			t.Errorf("mismatch")
+		}
+
+	}
+	if idx.Name() != f.Name() {
+		t.Errorf("names don't match")
+	}
+
+}
+func TestIndexWriteAboveLimit(t *testing.T) {
+
+	f, err := os.CreateTemp("", "index")
+	if err != nil {
+		t.Errorf("Errored")
+	}
+
+	c := Config{}
+	c.Segment.MaxIndexBytes = 1024
+
+	idx, err := newIndex(f, c)
+	if err != nil {
+		t.Fatal("couldn't create an index: \n", err)
+	}
+
+	maxEntries := c.Segment.MaxIndexBytes / entWidth
+
+	for i := 0; i < int(maxEntries); i++ {
+		err := idx.Write(uint32(i), uint64(3))
+		if err != nil {
+			t.Fatalf("should not have failed")
+		}
+	}
+	err = idx.Write(uint32(maxEntries+1), uint64(3))
+	if err == nil {
+		t.Errorf("should have failed")
+	}
+
+}
